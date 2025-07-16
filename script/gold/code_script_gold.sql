e0e98b2cc65caf3aecb047f7b4e4fba72b91da08
ValdesFeudjio <valdesfeudjio@gmail.com>
2025-07-16 18:24:32 +0000

    ajout de la vue dim_products

--- script/gold/code_script_gold.sql
+++ script/gold/code_script_gold.sql
@@ -1,5 +1,5 @@
 
--- Dimension customer
+-- Dimension customers
 create or replace view gold.dim_customers as (
 select     row_number() over (order by cst_id) as customer_key,
            ci.cst_id as customer_id,
@@ -18,3 +18,35 @@ select     row_number() over (order by cst_id) as customer_key,
     from silver.crm_cust_info ci 
     left join silver.erp_cust_az12 ca on ci.cst_key=ca.cid 
     left join SILVER.ERP_LOC_A101 el on ci.cst_key=el.cid);
+
+
+
+
+
+    -- Dimension products
+create or replace view gold.dim_products as (
+    select 
+    row_number() over (order by pn.prd_start_dt, pn.prd_key) as product_key,
+    pn.prd_id as product_id,
+    pn.prd_key as product_number,
+    pn.prd_nm as product_name,
+    pn.cat_id as category_id,
+    pc.cat as category,
+    pc.subcat as subcategory,
+    pc.maintenance,
+    pn.prd_cost as cost,
+    pn.prd_line as product_line,
+    pn.prd_start_dt as start_date
+    from silver.crm_prd_info pn 
+    left join silver.erp_cat_g1v2 pc
+    on pn.cat_id=pc.id
+    where pn.prd_end_dt is null -- Filter out all historical data to keep only the current products
+);
+
+
+
+
+
+
+-- fact sales view
+    
